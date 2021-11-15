6b50124771c1bfbf0fd716905c98adb8724e277c
Stefan Schneider <mail@stefanschneider.me>
2021-11-15 22:53:59 +0100

    Add start game button, score, type hints

--- game/disco/minigames/earworm/Earworm.gd
+++ game/disco/minigames/earworm/Earworm.gd
@@ -24,56 +24,63 @@ var order
 var play_order = []
 var clicked_order = []
 
-# var b = "text"
+var score = 0
 
-func generate_order(num=1000):
+
+func generate_order(num=1000) -> Array:
 	var numbers = []
 	for i in num:
 		var number = buttons.keys()[randi() % buttons.keys().size()]
 		numbers.append(number)
 	return numbers
 
-func play_round(round_number):
+func play_round(round_number) -> void:
 	clicked_order = []
 	var current_order = order
 	play_order = current_order.slice(0, round_number)
 	print(play_order)
-	# play sounds
+	
+	# play back sequence
 	playback = true
 	for i in play_order:
 		buttons[i].emit_signal("pressed")
 		yield(get_tree().create_timer(1.0), "timeout")	
 	playback = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():	
+
+func _ready() -> void:
+	$Score.text = ""
 	order = generate_order(100)
-	play_round(round_number)
 	
-func end_round():
-	print(clicked_order)
+	
+func end_round() -> void:
 	if not is_sequence_correct():
 		game_over()
 	else:
 		if is_sequence_complete():
 			print("good job! playing next round")
+			increase_score()
 			yield(get_tree().create_timer(2.0), "timeout")	
 			round_number += 1
 			play_round(round_number)
 		
-func game_over():
+func game_over() -> void:
+	$Score.text = "Game over."
 	print("Game over")
 	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	
+func increase_score() -> void:
+	score += 1
+	$Score.text = "Score: " + str(score)
 
-func is_sequence_complete():
+
+func is_sequence_complete() -> bool:
 	if play_order.slice(0, round_number) == clicked_order:
 		return true
 	return false
 	
-func is_sequence_correct():
+
+func is_sequence_correct() -> bool:
 	var full_list = play_order.duplicate()
 	if clicked_order:
 		for i in clicked_order:
@@ -81,24 +88,31 @@ func is_sequence_correct():
 				return false
 	return true
 
-		
 
-func _on_Button1_pressed(human):
+func _on_Button1_pressed(human) -> void:
 	if not playback:
 		clicked_order.append(1)
 		end_round()
 
-func _on_Button2_pressed(human):
+
+func _on_Button2_pressed(human) -> void:
 	if not playback:
 		clicked_order.append(2)
 		end_round()
 
-func _on_Button3_pressed(human):
+
+func _on_Button3_pressed(human) -> void:
 	if not playback:
 		clicked_order.append(3)
 		end_round()
 
-func _on_Button4_pressed(human):
+
+func _on_Button4_pressed(human) -> void:
 	if not playback:
 		clicked_order.append(4)
 		end_round()
+
+
+func _on_StartGame_pressed() -> void:
+	play_round(round_number)
+	$StartGame.hide()
