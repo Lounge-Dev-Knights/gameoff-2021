282399d1cd0ed68c36b326165f37529eb3c6a5c9
gantneba <33327887+gantneba@users.noreply.github.com>
2021-11-18 20:31:46 +0100

    Disco Light Minigame
    Basic Jump and Run with Tileset and Main Menu
    v0.1

---
+++ game/disco/disco_light_minigame/source/Actors/Actor.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+class_name Actor
+
+const FLOOR_NORMAL: = Vector2.UP
+
+export var speed: = Vector2(500,0)
+export var gravity: = 2000
+var _velocity: = Vector2.ZERO
+
+	
---
+++ game/disco/disco_light_minigame/source/Actors/Enemy.gd
@@ -0,0 +1,2 @@
+extends "res://game/disco/disco_light_minigame/source/Actors/Actor.gd"
+
