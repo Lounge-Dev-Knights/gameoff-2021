08cc0b0353d698dac9840ad0a339f83ee85fc8ed
gantneba <33327887+gantneba@users.noreply.github.com>
2021-11-21 20:42:07 +0100

    added bugs
    2nd part of bug pictures

--- game/disco/disco_light_minigame/source/Actors/LaserBeam.gd
+++ game/disco/disco_light_minigame/source/Actors/LaserBeam.gd
@@ -3,7 +3,7 @@ extends Node2D
 const MAX_LENGTH = 300
 
 onready var beam = $LaserBeam
-onready var end = $LaserEnd
+onready var laserend = $LaserEnd
 onready var rayCast2D = $RayCast2D
 
 func _physics_process(delta: float) -> void:
@@ -12,9 +12,9 @@ func _physics_process(delta: float) -> void:
 	var max_cast_to = mouse_position.normalized() * MAX_LENGTH
 	rayCast2D.cast_to = max_cast_to
 	if rayCast2D.is_colliding():
-		end.global_position = rayCast2D.get_collision_point()
+		laserend.global_position = rayCast2D.get_collision_point()
 	else:
-		end.global_position = rayCast2D.cast_to
+		laserend.global_position = rayCast2D.cast_to
 	beam.rotation = rayCast2D.cast_to.angle()
-	beam.region_rect.end.x = end.position.length()
+	beam.region_rect.end.x = laserend.position.length()
 	
