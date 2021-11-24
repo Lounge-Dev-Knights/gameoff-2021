849d692f8cdae9dab1c43d0b97b0b5cd0c0f2229
gantneba <33327887+gantneba@users.noreply.github.com>
2021-11-24 17:01:54 +0100

    small update
    WHY i cannot move the player anymore??

--- game/disco/disco_light_minigame/source/Actors/LaserBeam.gd
+++ game/disco/disco_light_minigame/source/Actors/LaserBeam.gd
@@ -16,16 +16,16 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	totalDelta += delta
 	if totalDelta >= 0.08:
-		if interpolatedpoint.x <= laserend.global_position.x + 5 and interpolatedpoint.x >= laserend.global_position.x - 5:
+		if interpolatedpoint.x <= laserend.global_position.x + 8 and interpolatedpoint.x >= laserend.global_position.x - 8:
 			interpolatedpoint = _setRandomPoint()
 		#interpolatedpoint = lerp(interpolatedpoint, laserend.global_position, 0.1)
 		#rayCast2D.cast_to = interpolatedpoint #max_cast_to
-		rayCast2D.cast_to = rayCast2D.cast_to.linear_interpolate(interpolatedpoint, 0.1)
-		if rayCast2D.is_colliding():
-			laserend.global_position = rayCast2D.get_collision_point()
-		else:
-			laserend.global_position = rayCast2D.cast_to
-			print(rayCast2D.cast_to)
+		rayCast2D.cast_to = rayCast2D.cast_to.linear_interpolate(interpolatedpoint, 0.2)
+		#if rayCast2D.is_colliding():
+		#	laserend.global_position = rayCast2D.get_collision_point()
+		#else:
+		laserend.global_position = rayCast2D.cast_to
+		print(rayCast2D.cast_to)
 		beam.rotation = rayCast2D.cast_to.angle()
 		beam.region_rect.end.x = laserend.position.length()
 		totalDelta = 0
