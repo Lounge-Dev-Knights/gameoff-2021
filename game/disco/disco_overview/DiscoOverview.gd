ea9ae45a7faeee492e22c6955f29eadcdc143668
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2021-12-01 00:28:25 +0100

    Add UI Texture buttons.
    Add Background video to disco arrival.
    Add fade out/in to SceneLoader.

--- game/disco/disco_overview/DiscoOverview.gd
+++ game/disco/disco_overview/DiscoOverview.gd
@@ -10,7 +10,7 @@ onready var background = $CanvasLayer/TextureRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	SceneLoader.fade_color = Color.black
 	MusicEngine.play_song("Club1")
 
 
--- game/intro/DiscoArrival.gd
+++ game/intro/DiscoArrival.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 
+export(float) var cutoff := 100.0 setget _set_cutoff
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -9,6 +11,8 @@ onready var name_card = $CanvasLayer/CenterContainer/CenterContainerRight/NameCa
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	AudioServer.set_bus_effect_enabled(AudioServer.get_bus_index("Music"), 0, true)
+	MusicEngine.play_song("Club1")
 	
 	Dialogic.set_variable("accessory", "Bow Tie")
 	var dialog = Dialogic.start("Party Arrival")
@@ -17,6 +21,9 @@ func _ready():
 	name_card.show()
 	
 
+func _exit_tree() -> void:
+	AudioServer.set_bus_effect_enabled(AudioServer.get_bus_index("Music"), 0, false)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -29,4 +36,19 @@ func _on_name_entered():
 	var dialog = Dialogic.start("Name Entered")
 	add_child(dialog)
 	yield(dialog, "timeline_end")
+	$AnimationPlayer.play("walk_in")
+	
+	yield($AnimationPlayer, "animation_finished")
+	SceneLoader.fade_color = Color(10, 10, 10, 1.0)
 	SceneLoader.goto_scene("res://game/disco/disco_overview/DiscoOverview.tscn")
+
+
+func _on_VideoPlayer_finished() -> void:
+	print("finished")
+	$Background/VideoPlayer.play()
+
+
+func _set_cutoff(new_value: float) -> void:
+	var lowpass: AudioEffectLowPassFilter = AudioServer.get_bus_effect(AudioServer.get_bus_index("Music"), 0)
+	lowpass.cutoff_hz = new_value
+	cutoff = new_value
