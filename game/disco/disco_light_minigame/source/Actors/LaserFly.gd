e3e485bc320fbe84f13bb85fe4e7bb90acc5f9e0
gantneba <gantneba@gmail.com>
2021-12-01 22:17:04 +0100

    game works so far
    :)
    updated bugs ;)

--- game/disco/disco_light_minigame/source/Actors/LaserFly.gd
+++ game/disco/disco_light_minigame/source/Actors/LaserFly.gd
@@ -1,2 +1,4 @@
 extends "res://game/disco/disco_light_minigame/source/Actors/ActorBug.gd"
 
+export var laserenergy = 1
+
--- game/disco/disco_light_minigame/source/Levels/LMG_Level_1.gd
+++ game/disco/disco_light_minigame/source/Levels/LMG_Level_1.gd
@@ -11,8 +11,12 @@ var TotalLightPercentage: = 0
 
 var countCurrentBugLight = 0.0
 var countTotalBugLight = 0.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Game_Over.visible = false
+	for lightBugCounters in get_tree().get_nodes_in_group("light_bugs"):
+			countCurrentBugLight += lightBugCounters.current_light_hp
 	for lightBugCounters in get_tree().get_nodes_in_group("light_bugs"):
 		countTotalBugLight += lightBugCounters.max_light_hp
 	$GameTimer.start(GameTime)
@@ -26,16 +30,27 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	TimeCounter += delta
 	if TimeCounter >= 1.0:
-		TotalLightPercentage = (1.0 * float(countCurrentBugLight) / countTotalBugLight)
+		
+		print("countCurrentBugLight:")
+		print(countCurrentBugLight)
+		print("countTotalBugLight:")
+		print(countTotalBugLight)
+		TotalLightPercentage = countCurrentBugLight/countTotalBugLight*100
+		print("TotaleLightPercentage:")
+		print(TotalLightPercentage)
 		_time_left = int($GameTimer.time_left)
 		$GameInfo/CanvasLayer/HBoxContainer/NameCard/VBoxContainer/HBoxContainer2/Time.text = str(_time_left)
 		$GameInfo/CanvasLayer/HBoxContainer/NameCard/VBoxContainer/HBoxContainer/Light.text = str(TotalLightPercentage)
 		TimeCounter = 0
 		countCurrentBugLight = 0
-	for lightBugCounters in get_tree().get_nodes_in_group("light_bugs"):
-		countCurrentBugLight += lightBugCounters.current_light_hp
+		for lightBugCounters in get_tree().get_nodes_in_group("light_bugs"):
+			countCurrentBugLight += lightBugCounters.current_light_hp
+	
 
 func _on_GameTimer_timeout() -> void:
 	get_tree().paused = true
+	TotalScore.light_score = TotalLightPercentage
+	$Game_Over/CenterContainer/NameCard/VBoxContainer/Score.text = str(TotalLightPercentage)
+	$Game_Over.visible = true
 	pass # Replace with function body.
-
+	
